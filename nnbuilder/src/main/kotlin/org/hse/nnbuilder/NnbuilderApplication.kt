93f34857f2e8aa0f09745727c21247a967f18b87
Timur Iskhakov <iskhakovt@gmail.com>
2022-03-04 16:02:14 +0530

    fix spaces

--- nnbuilder/src/main/kotlin/org/hse/nnbuilder/NnbuilderApplication.kt
+++ nnbuilder/src/main/kotlin/org/hse/nnbuilder/NnbuilderApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 open class NnbuilderApplication
 
 fun main(args: Array<String>) {
-	runApplication<NnbuilderApplication>(*args)
+    runApplication<NnbuilderApplication>(*args)
 }
