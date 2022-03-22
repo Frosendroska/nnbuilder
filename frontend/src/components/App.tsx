3a2345a8d98a8bad51252e8b8e51bf37ab16679e
Timur Iskhakov <iskhakovt@gmail.com>
2022-03-22 08:14:48 +0000

    Use react useCallback hook (#8)
    * Use react useCallback hook
    
    * style nit
    
    * parseInt

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,45 +1,49 @@
-import React, {useState} from 'react'
+import React, {useCallback, useState} from 'react'
 import * as api from '../clients'
 
 export default function App(): JSX.Element {
-    const [sum, setSum] = useState<number | undefined>(undefined)
-    const [lhs, setLhs] = useState<number | undefined>(undefined)
-    const [rhs, setRhs] = useState<number | undefined>(undefined)
+    const [sum, setSum] = useState<number | undefined>()
+    const [lhs, setLhs] = useState<number | undefined>()
+    const [rhs, setRhs] = useState<number | undefined>()
 
-    return <div>
-        <h1>Sum Calculator</h1>
-        <form onSubmit={(event) => {
-            event.preventDefault()
+    const onSubmit = useCallback((event) => {
+        event.preventDefault()
 
-            if (lhs === undefined || rhs === undefined) return
+        if (lhs === undefined || rhs === undefined) return
 
-            const request = new api.GetSumRequest()
-                .setLhs(lhs)
-                .setRhs(rhs)
-            api.sumService.getSum(request).then((value: api.GetSumResponse) => {
-                setSum(value.getSum())
-            })
-        }}>
-            <label>
-                lhs:
-                <input
-                    type="number"
-                    value={lhs || ''}
-                    onChange={(event) => setLhs(Number(event.target.value))}
-                />
-            </label>
-            <br />
-            <label>
-                rhs:
-                <input
-                    type="number"
-                    value={rhs || ''}
-                    onChange={(event) => setRhs(Number(event.target.value))}
-                />
-            </label>
-            <br />
-            <input type="submit" value="Compute sum" />
-        </form>
-        <h2>Computed sum: {sum}</h2>
-    </div>
+        const request = new api.GetSumRequest()
+            .setLhs(lhs)
+            .setRhs(rhs)
+        api.sumService.getSum(request).then((value: api.GetSumResponse) => {
+            setSum(value.getSum())
+        })
+    }, [lhs, rhs])
+
+    return (
+        <div>
+            <h1>Sum Calculator</h1>
+            <form onSubmit={onSubmit}>
+                <label>
+                    lhs:
+                    <input
+                        type="number"
+                        value={lhs || ''}
+                        onChange={(event) => setLhs(parseInt(event.target.value))}
+                    />
+                </label>
+                <br />
+                <label>
+                    rhs:
+                    <input
+                        type="number"
+                        value={rhs || ''}
+                        onChange={(event) => setRhs(parseInt(event.target.value))}
+                    />
+                </label>
+                <br />
+                <input type="submit" value="Compute sum" />
+            </form>
+            <h2>Computed sum: {sum}</h2>
+        </div>
+    )
 }
