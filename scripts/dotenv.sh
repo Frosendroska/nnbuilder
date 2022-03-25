77aed755297aa1889c76a9861ce2c2d9c230a99c
Timur Iskhakov <iskhakovt@gmail.com>
2022-03-25 13:04:53 +0000

    update script

--- scripts/dotenv.sh
+++ scripts/dotenv.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
 
+set -eu
+
 if [ -f "$(pwd)/$1" ]; then
-  set -o allexport
-  # shellcheck disable=SC1090
-  . "$(pwd)/$1"
-  set +o allexport
-else
   echo 'Environment configuration file does not exist'
+  exit 0
 fi
+
+set -o allexport
+# shellcheck disable=SC1090
+. "$(pwd)/$1"
+set +o allexport
 
 envsubst < "$2" > "$3"
