5fdb7afa584a0d45120426004d2e7bbdec7421a0
Timur Iskhakov <iskhakovt@gmail.com>
2022-03-25 13:13:14 +0000

    use abs path

--- scripts/dotenv.sh
+++ scripts/dotenv.sh
@@ -2,14 +2,14 @@
 
 set -eu
 
-if [ -f "$(pwd)/$1" ]; then
+if [ -f "$1" ]; then
   echo 'Environment configuration file does not exist'
   exit 0
 fi
 
 set -o allexport
 # shellcheck disable=SC1090
-. "$(pwd)/$1"
+. "$1"
 set +o allexport
 
 envsubst < "$2" > "$3"
