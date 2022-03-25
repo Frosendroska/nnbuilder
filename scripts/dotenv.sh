4b37be4199ee022b89bf098cc179392db506d846
Timur Iskhakov <iskhakovt@gmail.com>
2022-03-25 13:27:18 +0000

    Update script

--- scripts/dotenv.sh
+++ scripts/dotenv.sh
@@ -2,14 +2,19 @@
 
 set -eu
 
-if [ ! -f "$1" ]; then
-  echo 'Environment configuration file does not exist'
-  exit 0
+if [ $# -ne 3 ]; then
+  echo 'Usage: dotenv.sh DOT_ENV_FILE IN_FILE OUT_FILE'
+  echo 'If the DOT_ENV_FILE does not exist, script skips its load.'
+  exit 1
 fi
 
-set -o allexport
-# shellcheck disable=SC1090
-. "$1"
-set +o allexport
+if [ -f "$1" ]; then
+  set -o allexport
+  # shellcheck disable=SC1090
+  . "$1"
+  set +o allexport
+else
+  echo 'Environment configuration file does not exist'
+fi
 
 envsubst < "$2" > "$3"
