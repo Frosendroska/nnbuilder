3e0bd5a807010c61b84ece9e1f6ff368fabf5540
ancharts <annamikhaylova6263@gmail.com>
2022-04-18 01:01:51 +0300

    Change index.tsx for form

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import * as api from './clients'
+import Form from './components/Form'
 
 ReactDOM.render(
-    <App sumService={api.sumService} />,
+    //<App sumService={api.sumService} />,
+    <Form />,
     document.getElementById('root'),
 )
