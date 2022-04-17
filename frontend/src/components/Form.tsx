27deee8d2487b6d9ef8345cc436dfbd603e0d7c5
ancharts <annamikhaylova6263@gmail.com>
2022-04-18 01:00:37 +0300

    Add registration form

---
+++ frontend/src/components/Form.tsx
@@ -0,0 +1,55 @@
+import { SetStateAction, useState} from 'react';
+
+export default function Form() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleName = (e: { target: { value: SetStateAction<string>; }; }) => {
+        setName(e.target.value);
+    };
+
+    const handleEmail = (e: { target: { value: SetStateAction<string>; }; }) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePassword = (e: { target: { value: SetStateAction<string>; }; }) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+    };
+
+    const handleSubmitted = (e: { preventDefault: () => void; }) => {
+        alert('A name was submitted: ' + name);
+        e.preventDefault();
+    }
+
+    return (
+        <div className="form">
+            <div>
+                <h1>User Registration</h1>
+            </div>
+
+            <form>
+                <label className="label">Name</label>
+                <input onChange={handleName} className="input"
+                       value={name} type="text" />
+
+                <label className="label">Email</label>
+                <input onChange={handleEmail} className="input"
+                       value={email} type="email" />
+
+                <label className="label">Password</label>
+                <input onChange={handlePassword} className="input"
+                       value={password} type="password" />
+
+                <button onClick={handleSubmit} className="btn" type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+}
