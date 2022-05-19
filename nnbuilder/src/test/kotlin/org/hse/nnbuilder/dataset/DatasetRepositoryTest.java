671a6f5d545b91306990d01cb21be41a3b3ba4a0
Frosendroska <katyabraun0@gmail.com>
2022-05-19 23:51:27 +0300

    i dunno why it is not working

---
+++ nnbuilder/src/test/kotlin/org/hse/nnbuilder/dataset/DatasetRepositoryTest.java
@@ -0,0 +1,17 @@
+package org.hse.nnbuilder.dataset;
+
+import java.io.File;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+@ActiveProfiles("test")
+public class TasksQueueRepositoryTest {
+
+    void testAdd(){
+        File datasetFile = new File("Resourses/2022_forbes_billionaires.csv");
+        DatasetStored datasetStored = new DatasetStored(datasetFile);
+    }
+}
