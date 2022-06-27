a16a04fcc1906876b8d08da8ca434c1f9ae6a3dc
ancharts <annamikhaylova6263@gmail.com>
2022-06-28 01:32:22 +0300

    Fix imports

--- nnbuilder/src/main/kotlin/org/hse/nnbuilder/user/User.kt
+++ nnbuilder/src/main/kotlin/org/hse/nnbuilder/user/User.kt
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.OneToMany
 import javax.persistence.Table
+import javax.persistence.OrderBy
 
 @Entity
 @Table(name = "users")
